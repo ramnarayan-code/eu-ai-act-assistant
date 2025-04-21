35abeb04c2bea5b724f3b960ae22830482f8f146
ramnarayan-code <132490097+ramnarayan-code@users.noreply.github.com>
2025-04-21 23:31:16 +0200

    app

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ from llama_index.llms.openai import OpenAI
 from tools import search_tool, weather_info_tool, hub_stats_tool
 from retriever import guest_info_tool
 
-os.environ["OPENAI_API_KEY"] = ""
+os.environ["OPENAI_API_KEY"] = "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"
 
 # Initialize the Hugging Face model
 llm = OpenAI(model="gpt-4o-mini")
